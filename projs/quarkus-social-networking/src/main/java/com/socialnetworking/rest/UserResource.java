68f2cb10c8ad6aef0046b971abafe84575500a7e
Gilson Vieira de Souza <gilszon@gmail.com>
2023-01-10 23:27:45 -0300

    Created GET to manage Users

--- projs/quarkus-social-networking/src/main/java/com/socialnetworking/rest/UserResource.java
+++ projs/quarkus-social-networking/src/main/java/com/socialnetworking/rest/UserResource.java
@@ -2,15 +2,26 @@ package com.socialnetworking.rest;
 
 import com.socialnetworking.dto.CreateUserRequest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser( CreateUserRequest createUserRequest){
+        return Response.ok(createUserRequest).build();
+    }
+
+    @GET
+    public Response listAll(){
         return Response.ok().build();
     }
 
